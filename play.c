617c3e76f241439a2cf59f3fffa92c179a9760dc
davidfalcen-bit <davidfalcen@gmail.com>
2025-12-14 17:12:37 +0100

    sync

--- play.c
+++ play.c
@@ -1,22 +1,26 @@
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 #include "board.h"
 
+typedef enum{
+    EAGLE,
+    TAILS
+}coin;
+
 static void seed_prng(void) {
-    static bool seed = false;
-    if (!seed) {
-        srand(time(NULL));
-        for (int i = 0; i < 50; i++)
-            rand();
-        seed = true;
-    }
+  static bool seed = false;
+  if (!seed) {
+    srand(time(NULL));
+    for (int i = 0; i < 50; i++)
+      rand();
+    seed = true;
+  }
 }
 
 int random(int minimum, int maxaimum) {
-    seed_prng();
-    return rand() / (RAND_MAX / (maxaimum - minimum + 1) + 1) + minimum;
+  seed_prng();
+  return rand() / (RAND_MAX / (maxaimum - minimum + 1) + 1) + minimum;
 }
 
 void computer_logic(big_board *board, tic_tac_toe comp) {
@@ -109,14 +113,31 @@ void computer_logic(big_board *board, tic_tac_toe comp) {
     }
 }
 
-bool first_move_user(size_t n, big_board *boards, tic_tac_toe symbol_for_moving) {
-    if(boards->next_col == -1 && boards->next_row == -1) {
-        if (n < 1 || n > 81) return false;
-        n--;
+
+bool move_user(size_t n, big_board *boards, tic_tac_toe symbol_for_moving){
         size_t big_r   = n / 27;
         size_t big_c   = (n / 9) % 3;
         size_t small_r = (n / 3) % 3;
         size_t small_c = n % 3;
-        boards[big_r][big_c]->cells[small_r][small_c] = symbol_for_moving;
+    if(boards->next_col == -1 && boards->next_row == -1){
+        n--;
+        if(boards->boards[big_r][big_c].cells[small_r][small_c] != EMPTY) return false;
+        if (boards->boards[big_r][big_c].winner != EMPTY) return false;
+        boards->boards[big_r][big_c].cells[small_r][small_c] = symbol_for_moving;
+        boards->next_col = small_c;
+        boards->next_row = small_r;
+        return true;
+    }else{
+        size_t start = (boards->next_row * 27) + (boards->next_col * 9) + 1;
+        size_t end = start + 8;
+        if(n > end || n < start) return false;
+        if(boards->boards[big_r][big_c].cells[small_r][small_c] != EMPTY) return false;
+        if (boards->boards[big_r][big_c].winner != EMPTY) return false;
+        boards->boards[big_r][big_c].cells[small_r][small_c] = symbol_for_moving;
+        boards->next_col = small_c;
+        boards->next_row = small_r;
+        return true;
     }
 }
+
+
